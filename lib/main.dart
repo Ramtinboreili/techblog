c12a90635514c4c76845fd076b246a3d933ba806
Ramtinboreili <ramtin.bor7hp@gmail.com>
2024-09-13 20:10:33 +0330

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
